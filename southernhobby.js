2ffecf7d02fbea1ab00d95eee3f8ba7164831732
mac <ahmadrazzaq@gmail.com>
2026-04-15 21:40:58 +0500

    3 commit

--- southernhobby.js
+++ southernhobby.js
@@ -106,6 +106,8 @@ app.get("/run-southern", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
